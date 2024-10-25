ae75bb5c82e6c295ef2c471d5312c58ef56b48ac
Gerben Aaltink <gerbenaaltink@gmail.com>
2024-10-25 07:55:44 +0200

    Reformatted

--- test1.py
+++ test1.py
@@ -4,6 +4,7 @@ import libsql_client
 import time
 from pprint import pprint as pp
 
+
 class ResultObject:
     def __init__(self, result_set=None, **kwargs):
         self.__dict__.update(
@@ -76,15 +77,16 @@ class SQLClient:
         obj.total_duration = time.time() - time_start
         return obj
 
+
 class Bench:
     def __init__(self, title=None):
-        self.title = title 
-        self.time_start = None 
+        self.title = title
+        self.time_start = None
         self.time_end = None
-        self.duration = None 
+        self.duration = None
 
     def reset(self):
-        self.time_end = None 
+        self.time_end = None
         self._duration = None
         self.time_start = time.time()
 
@@ -98,39 +100,44 @@ class Bench:
         if not self._duration:
             self.time_end = time.time()
             self._duration = time.time() - self.time_start
-        return self._duration  
-    
+        return self._duration
+
     @duration.setter
     def duration(self, value):
         self._duration = value
-    
+
     def __exit__(self, *args):
-        self.duration 
+        self.duration
         print(f"-> {self.title} took {self.duration.__round__(3)} seconds.")
+
 
 async def execute_bench(url, record_count=5000):
     bench_start = time.time()
-    
+
     async with SQLClient(url) as client:
 
         # Drop table
         if (await client.execute("drop table pony;")).success:
-            print("Table pony is deleted. Crashed previous time? Clean up didn't happen.")
+            print(
+                "Table pony is deleted. Crashed previous time? Clean up didn't happen."
+            )
         else:
             print("Table pony does not exist. That's expected.")
 
         # Create table
         with Bench("create table pony"):
-            await client.execute("create table pony (id INTEGER PRIMARY KEY AUTOINCREMENT, name, age, color)")
-            
+            await client.execute(
+                "create table pony (id INTEGER PRIMARY KEY AUTOINCREMENT, name, age, color)"
+            )
+
         # Success is False, table already exists
         with Bench("create table pony again. Should fail to pass"):
             assert (
                 await client.execute("create table pony (id, name, age, color)")
-            ).success is False 
-        
+            ).success is False
+
         # Check if table and sequence table is created, count should equal two.
-        # This part can break in case of server meaning there's delay in 
+        # This part can break in case of server meaning there's delay in
         # schema synchronization
         with Bench("checking if schema is updated"):
             while (await client.execute("SELECT * from sqlite_schema")).count != 2:
@@ -139,7 +146,9 @@ async def execute_bench(url, record_count=5000):
 
         # Prepare {record_count} amount of tasks. Default 5000
         tasks = []
-        with Bench(f"preparing insertion of {record_count} records by creating {record_count} tasks with insert statement."):
+        with Bench(
+            f"preparing insertion of {record_count} records by creating {record_count} tasks with insert statement."
+        ):
             for x in range(record_count):
                 tasks.append(
                     client.execute(
@@ -153,12 +162,16 @@ async def execute_bench(url, record_count=5000):
         with Bench("Executing {} tasks in parallel.".format(record_count)) as bench:
             await asyncio.gather(*tasks)
             duration_insert = bench.duration
-        
+
         time_waited = 0
         with Bench("waiting until all inserted data is available for read") as bench:
             while True:
-                records_inserted = (await client.execute("SELECT count(0) as c from pony", debug=True)).records[0]["c"]
-                print("Records insert check: {}/{}".format(records_inserted,record_count))
+                records_inserted = (
+                    await client.execute("SELECT count(0) as c from pony", debug=True)
+                ).records[0]["c"]
+                print(
+                    "Records insert check: {}/{}".format(records_inserted, record_count)
+                )
                 if records_inserted == record_count:
                     break
                 await asyncio.sleep(2)
@@ -177,13 +190,18 @@ async def execute_bench(url, record_count=5000):
             indent=2,
         )
 
+
 async def async_test(url):
     async with SQLClient(url) as client:
-        await client.execute("create table insert_test (id INTEGER PRIMARY KEY AUTOINCREMENT, description)")
-        await client.execute("insert into insert_test(description) VALUES (?)", ("test",))
+        await client.execute(
+            "create table insert_test (id INTEGER PRIMARY KEY AUTOINCREMENT, description)"
+        )
+        await client.execute(
+            "insert into insert_test(description) VALUES (?)", ("test",)
+        )
         result = (await client.execute("select * from insert_test")).count == 1
         await client.execute("drop table insert_test")
-        return result 
+        return result
 
 
 async def main():
@@ -191,22 +209,30 @@ async def main():
     url_server = "http://localhost:8080"
     url_file = "file:local.db"
 
-    # Conclusion of this test: most of the time, 
+    # Conclusion of this test: most of the time,
     # a just single inserted record is not directly
     # to read from server
     with Bench("checks synchronization speed server"):
-        for x in range(1,11):
-            print("Check {} if inserted record to server is directy available: {}".format(x, await async_test(url_server)))
+        for x in range(1, 11):
+            print(
+                "Check {} if inserted record to server is directy available: {}".format(
+                    x, await async_test(url_server)
+                )
+            )
     input("Press enter to continue")
 
-    # Conclusion of this test: always, a just single 
+    # Conclusion of this test: always, a just single
     # inserted record is directly readable from the file
     with Bench("checks synchronization speed file"):
-        for x in range(1, 11): 
-            print("Check {} if inserted record to file is directy available: {}".format(x, await async_test(url_file)))
+        for x in range(1, 11):
+            print(
+                "Check {} if inserted record to file is directy available: {}".format(
+                    x, await async_test(url_file)
+                )
+            )
     input("Press enter to continue")
 
-    # We're gonna put 5000 records in the database using the server 
+    # We're gonna put 5000 records in the database using the server
     duration_server = await execute_bench(url_server)
     print(f"server: {duration_server}")
     # We're gonna put 5000 records in the database using the file
@@ -214,7 +240,6 @@ async def main():
     print("TOTAL RESULTS:")
     print(f"server: {duration_server}")
     print(f"file: {duration_file}")
-
 
 
 asyncio.run(main())
